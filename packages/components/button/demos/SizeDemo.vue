3daf7365d99b59ba4ed7f473bde95562190435df
chengbotao <chengbotao5221@163.com>
2025-05-24 04:49:31 +0000

    Deploying to learndo-ui from @ chengbotao/learndo-ui@e1a7832708e97828537993285c7181ba620df87c 🚀

---
+++ packages/components/button/demos/SizeDemo.vue
@@ -0,0 +1,5 @@
+<template>
+  <ld-button size="large">大号按钮</ld-button>
+  <ld-button>默认按钮</ld-button>
+  <ld-button size="small">小号按钮</ld-button>
+</template>
---
+++ packages/components/button/demos/StatusDemo.vue
@@ -0,0 +1,5 @@
+<template>
+  <ld-button disabled>默认按钮</ld-button>
+  <ld-button btn-type="primary" disabled>主要按钮</ld-button>
+  <ld-button btn-type="danger" disabled>危险按钮</ld-button>
+</template>
